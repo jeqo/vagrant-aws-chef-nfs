8ba3cfddab1d3ee62d491b9258cde2fffdb3ccf0
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-11-25 11:11:21 -0500

    fix wrappign

--- chef-repo/cookbooks/nfs-utils/recipes/nfs-client.rb
+++ chef-repo/cookbooks/nfs-utils/recipes/nfs-client.rb
@@ -4,7 +4,9 @@ directory "#{node['nfs-client']['local-directory']}" do
   action :create
 end
 
-command_mount = "mount -t nfs -o nfsvers=4 #{node['nfs-client']['server-host']}:#{node['nfs-client']['remote-directory']} #{node['nfs-client']['local-directory']}"
+command_mount = "mount -t nfs -o nfsvers=4
+#{node['nfs-client']['server-host']}:#{node['nfs-client']['remote-directory']}
+#{node['nfs-client']['local-directory']}"
 
 execute command_mount do
   action :run
